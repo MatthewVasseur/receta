2b09f9cbb70e71810cbc2f509a0b7c1d7aea7111
Matthew Vasseur <mattvasseur@gmail.com>
2014-11-23 19:53:41 -0500

    another search test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper.rb'
 feature "Looking up recipes", js: true do
   scenario "finding recipes" do
     visit '/'
+    # Test "baked"
     fill_in "keywords", with: "baked"
     click_on "Search"
 
     expect(page).to have_content("Baked Potato")
+    expect(page).to have_content("Baked Brussel Sprouts")
+    
+    # Test "brus"
+    fill_in "keywords", with: "brus"
+    click_on "Search"
+
     expect(page).to have_content("Baked Brussel Sprouts")
   end
 end
