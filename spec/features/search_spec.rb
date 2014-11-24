354b967b32be1aa66c8881e8c67f317b88c58a53
Matthew Vasseur <mattvasseur@gmail.com>
2014-11-23 23:34:48 -0500

    final searching

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper.rb'
 
 feature "Looking up recipes", js: true do
+  before do
+    Recipe.create!(name: 'Baked Potato w/ Cheese')
+    Recipe.create!(name: 'Garlic Mashed Potatoes')
+    Recipe.create!(name: 'Potatoes Au Gratin')
+    Recipe.create!(name: 'Baked Brussel Sprouts')
+  end
+  
   scenario "finding recipes" do
     visit '/'
     # Test "baked"
@@ -9,7 +16,7 @@ feature "Looking up recipes", js: true do
 
     expect(page).to have_content("Baked Potato")
     expect(page).to have_content("Baked Brussel Sprouts")
-    
+
     # Test "brus"
     fill_in "keywords", with: "brus"
     click_on "Search"
